1cae65cb141f059d0803cf198e5ff81ee5261602
root <root@test.vova-support.of.by>
2015-10-19 23:19:45 +0300

    change jboss home attribute and jboss server installing added in default recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,3 +13,32 @@ jboss_user = node['jboss']['user']
 jboss_home = node['jboss']['home']
 jboss_version = node['jboss']['version']
 jboss_url = node['jboss']['url']
+
+
+user jboss_user do
+  comment 'jboss_user user'
+end
+
+jboss_tar = jboss_url.split('/')[-1] + ".tar.gz"
+
+remote_file "/tmp/#{jboss_tar}" do
+  source jboss_url
+  notifies :run, "bash[install_program]", :immediately
+end
+
+bash "install_program" do
+  user "root"
+  cwd "/tmp"
+  code <<-EOH
+    tar -zxf #{jboss_tar}
+    mv #{jboss_tar} #{jboss_home}
+    rm -f jboss-as-7.1.1.Final.tar.gz    
+  EOH
+  action :nothing
+end
+
+directory jboss_home do
+  group jboss_user
+  owner jboss_user
+  mode "0755"
+end
