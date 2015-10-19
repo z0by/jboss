1efa61010a0268c6e74495f5b5f256938820f1d8
root <root@test.vova-support.of.by>
2015-10-19 21:06:48 +0300

    change information in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,6 +1,6 @@
 name             'jboss'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Nekhai Vova'
+maintainer_email 'nehaivova@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures jboss'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
